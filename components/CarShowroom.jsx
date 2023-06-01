d010dc87e8085c6187e55e651995cf383e58eabe
Nurima Lumbantoruan <126258902+nurima3001@users.noreply.github.com>
2023-06-01 14:04:55 +0700

    CarShowroom.jsx

--- components/CarShowroom.jsx
+++ components/CarShowroom.jsx
@@ -1,165 +1,70 @@
-import React, { useState } from 'react';
-import { Grid, TextField } from '@mui/material';
-import Button from '@mui/material/Button';
+import React from 'react';
+import { useState } from 'react';
+import { Card, CardContent, Grid, Typography, Container, TextField, Button } from '@mui/material';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import { mobil } from './mobil';
+import Zilla from '../assets/Zilla.png';
 
-const mobil = [
-  {
-    id: 1,
-    nama: 'Civic Type R',
-    path: 'civic-type-r.jpg',
-    merek: 'Honda',
-    warna: 'Merah',
-    tipe: 'Sedan'
-  },
-  {
-    id: 2,
-    nama: 'Innova Venture',
-    path: 'innova-venture.jpg',
-    merek: 'Toyota',
-    warna: 'Putih',
-    tipe: 'Mini Bus'
-  },
-  {
-    id: 3,
-    nama: 'APV',
-    path: 'apv.jpg',
-    merek: 'Suzuki',
-    warna: 'Hitam',
-    tipe: 'Mini Bus'
-  },
-  {
-    id: 4,
-    nama: 'M4 Competition',
-    path: 'm4-competition.jpg',
-    merek: 'BMW',
-    warna: 'Biru Tua',
-    tipe: 'Sport Car'
-  },
-  {
-    id: 5,
-    nama: 'S Class 3000',
-    path: 's-class-3000.jpg',
-    merek: 'Mercedes',
-    warna: 'Abu-abu',
-    tipe: 'Sedan'
-  },
-  {
-    id: 6,
-    nama: 'Aventador',
-    path: 'aventador.jpg',
-    merek: 'Lamborghini',
-    warna: 'Kuning',
-    tipe: 'Sport Car'
-  },
-  {
-    id: 7,
-    nama: 'Almaz',
-    path: 'almaz.jpg',
-    merek: 'Wuling',
-    warna: 'Abu-abu',
-    tipe: 'SUV'
-  },
-  {
-    id: 8,
-    nama: 'Sigra',
-    path: 'sigra.jpg',
-    merek: 'Daihatsu',
-    warna: 'Hitam',
-    tipe: 'Mini Bus'
-  },
-  {
-    id: 9,
-    nama: 'Cullinan',
-    path: 'cullinan.jpg',
-    merek: 'Rolls Royce',
-    warna: 'Emas',
-    tipe: 'SUV'
-  },
-  {
-    id: 10,
-    nama: 'Macan',
-    path: 'macan.jpg',
-    merek: 'Porsche',
-    warna: 'Orange',
-    tipe: 'SUV'
-  },
-  {
-    id: 11,
-    nama: 'Veyron',
-    path: 'veyron.jpg',
-    merek: 'Bugatti',
-    warna: 'Hitam',
-    tipe: 'Sport Car'
-  },
-  {
-    id: 12,
-    nama: 'Aventador',
-    path: 'aventador-merah.jpg',
-    merek: 'Lamborghini',
-    warna: 'Merah',
-    tipe: 'Sport Car'
-  }
-];
-
-function CarList({ cars }) {
-  return (
-    <Grid container spacing={2}>
-      {cars.map(car => (
-        <Grid item xs={3} key={car.id}>
-          <div className="car">
-            <img src={car.path} alt={car.nama} />
-            <h3>{car.nama}</h3>
-            <p>Merek: {car.merek}</p>
-            <p>Warna: {car.warna}</p>
-            <p>Tipe: {car.tipe}</p>
-            <Button variant="contained" color="primary">
-              LIHAT DETAIL
-            </Button>
-            
-          </div>
-        </Grid>
-      ))}
-    </Grid>
-  );
-}
-
-function SearchBar({ onSearch }) {
+const CarShowroom = () => {
   const [searchTerm, setSearchTerm] = useState('');
-
-  const handleChange = event => {
-    setSearchTerm(event.target.value);
-    onSearch(event.target.value);
-  };
-
-  return (
-    <TextField
-      type="text"
-      label="Cari mobil..."
-      variant="outlined"
-      value={searchTerm}
-      onChange={handleChange}
-      fullWidth
-      margin="normal"
-    />
-  );
-}
-
-function CarShowroom() {
   const [searchResults, setSearchResults] = useState(mobil);
 
-  const handleSearch = searchTerm => {
-    const filteredResults = mobil.filter(car =>
-      car.nama.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-    setSearchResults(filteredResults);
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+    filterResults(event.target.value);
+  };
+
+  const filterResults = (searchTerm) => {
+    const results = mobil.filter((mobil) => mobil.nama.toLowerCase().includes(searchTerm.toLowerCase()));
+    setSearchResults(results);
   };
 
   return (
     <div>
-      <SearchBar onSearch={handleSearch} />
-      <CarList cars={searchResults} />
+      <Container maxWidth="lg">
+        <Grid item xs={12} sm={6}>
+          <div style={{ marginBottom: '1rem' }}>
+            <Typography variant="h2" color="#0599DE" style={{ marginBottom: '1rem' }} fontFamily={'Playfair Display'}>
+              <img src={Zilla} alt="Zilla" width="60px" />
+              <span style={{ marginLeft: '1rem' }}>Zilla Showroom</span>
+            </Typography>
+            <Typography variant="h5" style={{ marginBottom: '1rem' }} color="#0599DE" fontFamily={'Playfair Display'}>
+              Cari Mobil Impian Anda!
+            </Typography>
+            <TextField
+              style={{ width: '100%' }}
+              type="text"
+              variant="outlined"
+              placeholder="Search Mobil Anda..."
+              value={searchTerm}
+              onChange={handleChange}
+              InputProps={{
+                endAdornment: <SearchOutlinedIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />,
+              }}
+            />
+          </div>
+        </Grid>
+        <Grid container spacing={3}>
+          {searchResults.map((mobil) => (
+            <Grid item key={mobil.id} xs={12} sm={6} md={4} lg={3}>
+              <Card>
+              <img src={mobil.path} alt={mobil.nama} width="267" height="201" />           
+                <CardContent fontFamily={'Public Sans'}>
+                  <Typography variant="body1">Nama: {mobil.nama}</Typography>
+                  <Typography variant="body1">Merek: {mobil.merek}</Typography>
+                  <Typography variant="body1">Warna: {mobil.warna}</Typography>
+                  <Typography variant="body1">Tipe: {mobil.tipe}</Typography>
+                  <Button variant="contained" style={{ backgroundColor: '#0599DE', margin: '0.1rem' }}>
+                    Lihat Detail
+                  </Button>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
     </div>
   );
-}
+};
 
-export default CarShowroom;
+export default CarShowroom;
